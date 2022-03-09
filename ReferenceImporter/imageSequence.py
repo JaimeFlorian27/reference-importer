f62367c59a57ed212e1fa54d682060e7e5b865a3
jaimeforian27 <jaimeflorian27@gmail.com>
2022-03-08 22:17:59 -0800

    Fixed concat of output_dir, image seq

--- ReferenceImporter/imageSequence.py
+++ ReferenceImporter/imageSequence.py
@@ -15,14 +15,20 @@ class ImageSequencer():
     
     def createSequence(self):
         #Import Video
+        self.video_file.encode('Cp1252')
+        print(self.video_file)
         input = ffmpeg.input(self.video_file)
-        input.filter('fps',24, round='up')
+        input.filter('fps',self.output_frameRate, round='up')
 
         #Set Trimming
         #input.trim(start_frame = trim_start, end_frame = trim_end)
         
         #Create Image Sequence
+        self.output_dir.encode('Cp1252')
+        self.output_name = "test%03d.jpg"
         self.output_name = self.output_dir + '\\'+ self.output_name
+        self.output_name.encode('Cp1252')
+        print(self.video_file)
         output = ffmpeg.output(input, self.output_name)
         try:
             output.run(capture_stdout=True, capture_stderr=True)
