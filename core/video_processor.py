da6a6f22f337ca1fc8556cfc0f992b42a9e3ea4b
Jaime Florian <jaimeflorian27@gmail.com>
2023-04-05 00:52:55 -0700

    feat: better readibility for qt connections, new create image sequence using ffmpeg layer.

--- core/video_processor.py
+++ core/video_processor.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-import os
-import subprocess
 import ffmpeg
 
 
 class VideoProcessor:
-
     def __init__(self, padding="%03d", output_frameRate=24):
         self.output_frameRate = output_frameRate
         self.padding = padding
@@ -33,7 +30,7 @@ class VideoProcessor:
 
         video = ffmpeg.probe(video_file)
         # Extract the duration in seconds
-        duration = float(video['format']['duration'])
+        duration = float(video["format"]["duration"])
 
         hours = int(duration // 3600)
         minutes = int((duration % 3600) // 60)
@@ -41,28 +38,52 @@ class VideoProcessor:
         milliseconds = int((duration % 1) * 1000)
 
         # Format the duration in HH:MM:SS.ms format
-        formatted_duration = '{:02d}:{:02d}:{:02d}.{:03d}'.format(hours,
-                                                                  minutes,
-                                                                  seconds,
-                                                                  milliseconds)
+        formatted_duration = "{:02d}:{:02d}:{:02d}.{:03d}".format(
+            hours, minutes, seconds, milliseconds
+        )
         return formatted_duration
 
-    def create_image_sequence(self, input_file, frameRate, start_trim, end_trim, output_file):
-        command = (
-            '"%s" -i "%s" -r %s -vf scale=1280:-1 -q:v 3 -ss %s -to %s "%s"'
-            % (
-                self.ffmpeg_path,
-                input_file,
-                frameRate,
-                start_trim,
-                end_trim,
-                output_file,
-            )
-        ).encode(self.encoding)
-        try:
-            subprocess.call(command)
-        except Exception as e:
-            raise e
+    def create_image_sequence(
+        self, input_file, frame_rate, start_trim, end_trim, output_file
+    ):
+        """
+        Creates an image sequence from a video file using FFmpeg.
+
+        Args:
+            input_file (str): Path to the input video file.
+
+            frame_rate (int): Frame rate of the output image sequence.
+
+            start_trim (Optional[str]): Start timecode for trimming the input
+            video file (default: None).
+
+            end_trim (Optional[str]): End timecode for trimming the input video
+            file (default: None).
+
+            output_file (str): Path to the output image sequence file. The file
+            path should include the image file pattern,
+            e.g., '/path/to/output_file_%04d.png'.
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: If the output file path doesn't include the image file
+            pattern.
+
+        Example:
+            >>> create_image_sequence('/path/to/input_file.mp4',
+                                      30, start_trim='00:00:10',
+                                      end_trim='00:01:30',
+                                      output_file='/path/output_file_%04d.png')
+        """
+        (
+            ffmpeg.input(input_file, ss=start_trim, to=end_trim)
+            .filter("scale", width=1280, height=-1)
+            .filter("fps", fps=frame_rate)
+            .output(output_file, q=3)
+            .run()
+        )
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -31,16 +31,54 @@ class ReferenceImporterDialog(QDialog):
         self.ui.pushButton_create_image_sequence.setDisabled(True)
 
     def create_connections(self):
-        self.ui.pushButton_fileExplorer_input.clicked.connect(self.set_input)
-        self.ui.pushButton_fileExplorer_output.clicked.connect(self.set_output)
-        self.ui.pushButton_create_image_sequence.clicked.connect(
-            self.create_image_sequence
+        (
+            self.ui
+            .pushButton_fileExplorer_input
+            .clicked
+            .connect(self.set_input)
         )
-        self.ui.lineEdit_video_path.textChanged.connect(self.validate_fields)
-        self.ui.lineEdit_sequence_file_name.textChanged.connect(self.validate_fields)
-        self.ui.lineEdit_start_trim.textChanged.connect(self.validate_fields)
-        self.ui.lineEdit_end_trim.textChanged.connect(self.validate_fields)
-        self.ui.lineEdit_output_directory.textChanged.connect(self.validate_fields)
+        (
+            self.ui
+            .pushButton_fileExplorer_output
+            .clicked
+            .connect(self.set_output)
+        )
+        (
+            self.ui
+            .pushButton_create_image_sequence
+            .clicked
+            .connect(self.create_image_sequence)
+        )
+        (
+            self.ui
+            .lineEdit_video_path
+            .textChanged
+            .connect(self.validate_fields)
+        )
+        (
+            self.ui
+            .lineEdit_sequence_file_name
+            .textChanged
+            .connect(self.validate_fields)
+        )
+        (
+            self.ui
+            .lineEdit_start_trim
+            .textChanged
+            .connect(self.validate_fields)
+        )
+        (
+            self.ui
+            .lineEdit_end_trim
+            .textChanged
+            .connect(self.validate_fields)
+        )
+        (
+            self.ui
+            .lineEdit_output_directory
+            .textChanged
+            .connect(self.validate_fields)
+        )
 
     def set_input(self):
         filename = self.ui.lineEdit_video_path.text()
@@ -91,7 +129,7 @@ class ReferenceImporterDialog(QDialog):
             self.ui.pushButton_create_image_sequence.setEnabled(False)
 
     def validate_timecode(self, text):
-        timecode_rx = QtCore.QRegExp("([0-9]{2}[:]){1,2}[0-9]{2}[.]?[0-9]{0,2}")
+        timecode_rx = QtCore.QRegExp("([0-9]{2}[:]){1,2}[0-9]{2}[.]?[0-9]+")
         timecode_validator = QtGui.QRegExpValidator(timecode_rx, self)
         state = timecode_validator.validate(text, 0)
         if state[0] == QtGui.QRegExpValidator.Acceptable:
@@ -102,7 +140,7 @@ class ReferenceImporterDialog(QDialog):
 
     def create_image_sequence(self):
         try:
-            frameRate = str(self.ui.spinBox_target_frame_rate.value())
+            frame_rate = str(self.ui.spinBox_frame_rate.value())
 
             input_file = os.path.normpath(self.ui.lineEdit_video_path.text())
 
@@ -110,15 +148,15 @@ class ReferenceImporterDialog(QDialog):
             trim_end = str(self.ui.lineEdit_end_trim.text())
 
             output_dir = os.path.normpath(self.ui.lineEdit_output_directory.text())
-            output_ext = self.ui.comboBox.currentText()
+            output_ext = self.ui.comboBox_file_extension.currentText()
 
             output_name = (
                 self.ui.lineEdit_sequence_file_name.text() + "_%03d" + output_ext
             )
             output_file = os.path.join(output_dir, output_name)
 
-            self.video_processor.createSequence(
-                input_file, frameRate, trim_start, trim_end, output_file
+            self.video_processor.create_image_sequence(
+                input_file, frame_rate, trim_start, trim_end, output_file
             )
 
             if self.ui.checkBox_image_plane.isChecked():
